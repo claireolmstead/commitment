97656f7bc563857d6c05b4bbbd09a42b525f6c34
Virginia Olmstead <viol5229@colorado.edu>
2020-01-30 12:05:37 -0700

    slideshow working

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
 console.log("Hello World");
-let i = 0;
-function slideshowNext(){
-	if(i == 0){
-		i++;
-	}else if(i == 1){
-		i++;
-	}else{
+var slideIndex = 1;
+showSlides(slideIndex);
 
-		i = 0;
-	} 
-});
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-function slideshowPrev(){
-	if(i == 2){
-		i--;
-	}else if(i == 1){
-		i--;
-	}else{
-		
-		i = 2;
-	} 
-});
-$(".prev").click(slideshowPrev);
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
