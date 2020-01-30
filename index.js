4ec9a5d35ec8d67d44e89aef4a8377886ef39b77
Virginia Olmstead <viol5229@colorado.edu>
2020-01-30 11:35:44 -0700

    starting js and css updates

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 console.log("Hello World");
+let i = 0;
+function slideshowNext(){
+	if(i == 0){
+		i++;
+	}else if(i == 1){
+		i++;
+	}else{
+
+		i = 0;
+	} 
+});
+
+function slideshowPrev(){
+	if(i == 2){
+		i--;
+	}else if(i == 1){
+		i--;
+	}else{
+		
+		i = 2;
+	} 
+});
+$(".prev").click(slideshowPrev);
